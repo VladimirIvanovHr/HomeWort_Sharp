1d13190640f343a4344e6bcc929783eef28dfc62
Vladimir Ivanov <ivanofvv9@gmail.com>
2022-10-02 22:32:47 +0300

    Задача на четный числа от 1 до N

---
+++ HW_1_4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число больше 0:");
+int N = Convert.ToInt32(Console.ReadLine());
+int number = 2;
+if (N<=0)
+{
+Console.WriteLine("неверное число");
+}
+else while (number <= N)
+{
+    Console.Write(number + ", ");
+    number = number + 2;
+}
+Console.WriteLine();
