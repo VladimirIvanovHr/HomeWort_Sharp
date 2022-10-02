f3fef99d6d18c5693074ee5caedfe8868dd0cd31
Vladimir Ivanov <ivanofvv9@gmail.com>
2022-10-02 22:03:37 +0300

    Задача на определение четности числа

---
+++ HW_1_3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Определим четность числа?");
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2==0)
+{
+Console.WriteLine("число четное");
+}
+else
+Console.WriteLine("Число нечетное");
