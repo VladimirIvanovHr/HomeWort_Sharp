72fadf47609ffac17cd473fd55029859f8b1c528
Vladimir Ivanov <ivanofvv9@gmail.com>
2022-10-02 21:27:40 +0300

    Задача на определение большего числа

---
+++ HW_1_1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Сравним два числа?");
+Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1==number2)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{
+    if (number1>number2)
+{
+    Console.WriteLine($"Больше число {number1}");
+}  
+    else
+{
+    Console.WriteLine($"Больше число {number2}");
+}
+}
